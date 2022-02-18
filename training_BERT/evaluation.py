4f030b087d72dfdf53dc983298d9df15f56f7971
Bjarki Viðar Kristjánsson <bvkr@kth.se>
2022-02-18 13:37:43 +0100

    createt an evaluation file

---
+++ training_BERT/evaluation.py
@@ -0,0 +1,104 @@
+import json
+from random import randrange
+from transformers import AutoTokenizer, AutoModelForMaskedLM, pipeline, AutoModelForSequenceClassification
+from transformers import TrainingArguments, Trainer
+import datasets
+import torch
+import torch.utils.data as data
+import numpy as np
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
+
+class WikiDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+# Load tokenizer and model
+tokenizer = AutoTokenizer.from_pretrained("KB/bert-base-swedish-cased")
+model = AutoModelForSequenceClassification.from_pretrained("KB/bert-base-swedish-cased")
+
+queries = []
+paragraphs = []
+labels = []
+
+
+
+# Real Dataset, read from paragraphs.json bulk upload file
+# Open the list of articles to read
+paragraphsfile = open('paragraphs_chunked_1_mini.json', 'r')
+lines = paragraphsfile.readlines()
+
+# Create positive examples (label = 1)
+for index, line in enumerate(lines):
+    line = line.replace('\n', '')
+    if(line != '{"index":{}}'): # We discard the empty lines
+        y = json.loads(line)
+        q = y["id1"] + " " + y["id2"] + " " + y["id3"] + " " + y["id4"] + " " + y["id5"] # Create a query that is all ids concatinated together with space between
+        query = q.strip() # Remove spaces at the end
+        queries.append(query)
+        paragraphs.append(y["paragraph"])
+        labels.append(1) # these are all positive examples, that is these paragraphs are relevant to the query
+
+pos_count = len(queries)
+
+# Create as many negative examples as positive (negative: label = 0)
+neg_count = pos_count
+neg_queries = []
+neg_paragraphs = []
+neg_labels = []
+for i in range(neg_count):
+    random_index = randrange(neg_count)
+    # Find a random paragraph that is paired with another query than its correct query
+    while (queries[random_index] == queries[i]): 
+        random_index = randrange(neg_count)
+    neg_queries.append(queries[i])
+    neg_paragraphs.append(paragraphs[random_index])
+    neg_labels.append(0) # these are all negative examples, that is these paragraphs are not relevant to the query
+
+
+queries.extend(neg_queries)
+paragraphs.extend(neg_paragraphs)
+labels.extend(neg_labels)
+
+
+
+
+
+# Will pad the sequences up to the model max length
+model_inputs = tokenizer(queries, paragraphs, truncation='longest_first', padding='max_length', max_length=512)
+
+
+
+dataset = WikiDataset(model_inputs, labels)
+
+
+train_set_size = int(len(dataset) * 0.8)
+val_set_size = len(dataset) - train_set_size
+train_dataset, val_dataset = data.random_split(dataset, [train_set_size, val_set_size], generator=torch.Generator().manual_seed(42))
+
+
+
+
+
+# Evaluation
+
+# saved_model = torch.load('model_B_10k')
+
+
+trainer = Trainer(
+    model=saved_model,                   # the instantiated 🤗 Transformers model to be trained
+    args=training_args,                  # training arguments, defined above
+    train_dataset=train_dataset,         # training dataset
+    eval_dataset=val_dataset,            # evaluation dataset
+)
+
+trainer.evaluate()
+
